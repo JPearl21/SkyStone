9b4894fe4ecec2a467a1003e3024eaca5d505f84
JPearl21 <43004905+JPearl21@users.noreply.github.com>
2020-01-15 15:50:37 -0500

    commit test

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpV2.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleop/TeleOpV2.java
@@ -6,7 +6,7 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import org.firstinspires.ftc.teamcode.hardware.MaccabotV2;
 
 @TeleOp(name="TeleOp v2")
-//@Disabled
+//@Disable
 public class TeleOpV2 extends OpMode {
 
     private MaccabotV2 robot;
