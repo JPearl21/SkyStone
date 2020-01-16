9f5bf752a043a72e1983e73e85716fd471182360
Owen <43004995+UnintelliJ@users.noreply.github.com>
2020-01-16 16:50:29 -0500

    During the 64" forward.
    The left side motors kinda die, and all movements become tokyo drift

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedCompetitionAutoBeta.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/auto/RedCompetitionAutoBeta.java
@@ -41,8 +41,8 @@ public class RedCompetitionAutoBeta extends LinearOpMode {
         telemetry.update();
 
 
-        // FORWARD MOVE
-        robot.drive.setTargetsTicks(MaccaDrive.inchesToEncoderTicks(24), MaccaDrive.inchesToEncoderTicks(24));
+        // FORWARD MOVE 32"
+        robot.drive.setTargetsTicks(MaccaDrive.inchesToEncoderTicks(30), MaccaDrive.inchesToEncoderTicks(30));
         timer.reset();
         robot.drive.runToTargets(0.9, 0.9);
         while (opModeIsActive() && robot.drive.isDriveBusy() && timer.seconds() < 3) {
@@ -50,6 +50,16 @@ public class RedCompetitionAutoBeta extends LinearOpMode {
         }
         robot.drive.setMotorPowers(0);
 
+        //TODO Arm CLose
+
+        //Back ~16"
+        robot.drive.setTargetsTicks(-MaccaDrive.inchesToEncoderTicks(16), -MaccaDrive.inchesToEncoderTicks(16));
+        timer.reset();
+        robot.drive.runToTargets(-0.9, -0.9);
+        while (opModeIsActive() && robot.drive.isDriveBusy() && timer.seconds() < 3) {
+            telemetry.update();
+        }
+        robot.drive.setMotorPowers(0);
 
         // RIGHT TURN
         robot.drive.setTargetsTicks(MaccaDrive.inchesToEncoderTicks((13.75 * 2 * Math.PI) / 4),
@@ -59,5 +69,43 @@ public class RedCompetitionAutoBeta extends LinearOpMode {
         while (opModeIsActive() && robot.drive.isDriveBusy() && timer.seconds() < 1.5) {
             telemetry.update();
         }
+        robot.drive.setMotorPowers(0);
+
+
+        // FORWARD MOVE 64"
+        robot.drive.setTargetsTicks(MaccaDrive.inchesToEncoderTicks(64), MaccaDrive.inchesToEncoderTicks(64));
+        timer.reset();
+        robot.drive.runToTargets(1, 0.9);
+        while (opModeIsActive() && robot.drive.isDriveBusy() && timer.seconds() < 5) {
+            telemetry.update();
+        }
+        robot.drive.setMotorPowers(0);
+
+
+
+
+
+
+
+        //Left Turn 90*
+        robot.drive.setTargetsTicks(-MaccaDrive.inchesToEncoderTicks((13.75 * 2 * Math.PI) / 4),
+                MaccaDrive.inchesToEncoderTicks((13.75 * 2 * Math.PI) / 4));
+        timer.reset();
+        robot.drive.runToTargets(-0.9, 0.9);
+        while (opModeIsActive() && robot.drive.isDriveBusy() && timer.seconds() < 1.5) {
+            telemetry.update();
+        }
+        robot.drive.setMotorPowers(0);
+
+        // FORWARD MOVE 6"
+        robot.drive.setTargetsTicks(MaccaDrive.inchesToEncoderTicks(6), MaccaDrive.inchesToEncoderTicks(6));
+        timer.reset();
+        robot.drive.runToTargets(0.9, 0.9);
+        while (opModeIsActive() && robot.drive.isDriveBusy() && timer.seconds() < 3) {
+            telemetry.update();
+        }
+        robot.drive.setMotorPowers(0);
+
+        //TODO A Drop
     }
 }
